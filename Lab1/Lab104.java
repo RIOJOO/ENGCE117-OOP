87c216152c18cfb0a0f742287e3f3317265a421e
RIOJOO <Surabordin2006@gmail.com>
2025-11-23 21:58:54 +0700

    update

--- Lab1/Lab104.java
+++ Lab1/Lab104.java
@@ -1,5 +1,18 @@
 package Lab1;
 
+import java.util.Scanner;
+
 public class Lab104 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int score = sc.nextInt();
+
+        String[] grades = {"F", "F", "F", "F", "F", "D", "C", "B", "A", "A", "A"};
+
+        int index = score / 10;
+        
+        System.out.println(grades[index]);
+        
+        sc.close();
+    }
 }
