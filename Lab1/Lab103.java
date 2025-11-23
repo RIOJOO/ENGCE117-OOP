552cc5672ea33a76c55bf4d41692f260d2c3b9dd
RIOJOO <Surabordin2006@gmail.com>
2025-11-23 21:45:20 +0700

    อัพเดท

---
+++ Lab1/Lab103.java
@@ -0,0 +1,18 @@
+package Lab1;
+
+import java.util.Scanner;
+
+public class Lab103 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        if ((n & 1) == 0) {
+            System.out.println("Even");
+        } else {
+            System.out.println("Odd");
+        }
+        
+        sc.close();
+    }
+}
