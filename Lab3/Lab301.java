7649f886884f1cb9c2473fa510bb4b3654f4ccb8
RIOJOO <Surabordin2006@gmail.com>
2025-12-14 10:56:47 +0700

    update

---
+++ Lab3/Lab301.java
@@ -0,0 +1,22 @@
+package Lab3;
+
+public class User { 
+
+    private String username;
+
+    public User(String username) {
+        this.username = username;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public static void main(String[] args) {
+        String userName = " Somchai ";
+
+        User userobject = new User(userName.trim()); 
+        
+        System.out.println("User name is: " + userobject.getUsername());
+    }
+}
