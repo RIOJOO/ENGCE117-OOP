ca8a9da892989e73233e7c49512b0c23753189e7
RIOJOO <Surabordin2006@gmail.com>
2025-12-14 11:07:10 +0700

    update

---
+++ Lab3/Main.java
@@ -0,0 +1,36 @@
+import java.util.Scanner; 
+
+class Player {
+
+    private int score;
+
+    public Player(int initialScore) {
+        this.score = initialScore;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int newScore) {
+        this.score = newScore;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int initialScore = scanner.nextInt();
+
+        int updatedScore = scanner.nextInt();
+
+        Player player = new Player(initialScore);
+
+        player.setScore(updatedScore);
+
+        System.out.println(player.getScore());
+        
+        scanner.close();
+    }
+}
