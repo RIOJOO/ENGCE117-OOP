27ea628d0797e1bb40247b0139836e193df772d4
RIOJOO <Surabordin2006@gmail.com>
2025-12-21 16:21:11 +0700

    update

---
+++ Lab4/Playermain.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+class Player {
+    private String username;
+    private int level;
+
+    public Player() {
+        this.username = "Guest";
+        this.level = 1;
+    }
+
+    public Player(String username, int level) {
+        this.username = username;
+        this.level = level;
+    }
+
+    public void displayProfile() {
+        System.out.println("User: " + username + ", Level: " + level);
+    }
+}
+
+public class PlayerMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int mode = sc.nextInt();
+        sc.nextLine();
+
+        if (mode == 1) {
+            Player p = new Player();
+            p.displayProfile();
+        } else if (mode == 2) {
+            String name = sc.nextLine();
+            int level = sc.nextInt();
+            Player p = new Player(name, level);
+            p.displayProfile();
+        }
+        
+        sc.close();
+    }
+}
